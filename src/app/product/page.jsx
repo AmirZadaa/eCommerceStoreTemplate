e63b770c9687c9d6b2bcae1dfc5f3eb4638f484a
Amir zada <zadaa3322@gmail.com>
2025-03-06 02:37:05 +0500

    testing error 4

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -5,12 +5,12 @@ export default function Page() {
   return (
     <div className='container mx-auto mb-28'>
       <div className="text-center p-4 bg-secondary  space-y-4 ">
-            {/* <h1 className="font-semibold text-2xl ">tan cargo shorts</h1>
-            <div className="text-md font-semibold ">Home/ product</div> */}
+            <h1 className="font-semibold text-2xl ">tan cargo shorts</h1>
+            <div className="text-md font-semibold ">Home/ product</div>
       </div>
       <div className='grid grid-cols-3 gap-8 px-20 max-[1200px]:grid-cols-2  max-md:grid-cols-1 max-sm:px-4 max-wide:px-6 mt-4'>
-       {/* <ProductImages/>
-       <ProductDetails/> */}
+       <ProductImages/>
+       <ProductDetails/>
        <AddToCard    />
       </div>
     </div>
